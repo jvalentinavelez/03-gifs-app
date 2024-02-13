5678adcfad2e3fb9728e7482ac59ca21fa09756b
Juanita Velez <jvalentina.velez@gmail.com>
2024-02-12 21:58:41 -0500

    localstorage, peticiones http, @viewchild

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
   private apiKey: string = '14jjHoMQTIVHX5GvmtF5ayuyGTe7PCLQ';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     //valor pasa por referencia, por tanto se debe hacer una copia
@@ -23,13 +30,49 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
-  public searchTag(tag: string): void {
+  private saveLocalStorage(): void {
+    //serializacion
+    //Se debe transformar el array tagsHisotry a un string
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    //se finaliza con ! para indicar que es non null y siempre va a venir con data
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  searchTag(tag: string): void {
     if (tag.length === 0) return;
 
     this.organizeHistory(tag);
 
-    // this._tagsHistory.unshift(tag);
+    //Mediante fetch
+    // const resp = await fetch(
+    //   'https://api.giphy.com/v1/gifs/search?api_key=14jjHoMQTIVHX5GvmtF5ayuyGTe7PCLQ&q=snoopy&limit=10'
+    // );
+    // const data = await resp.json();
+
+    //mediante http de angular
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    const resp = this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+        //console.log({ gifs: this.gifList });
+      });
   }
 }
